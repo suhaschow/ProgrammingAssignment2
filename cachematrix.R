9020db741876e1063c11e28302b09bfaa4406e35
Suhas <c_suhas@yahoo.com>
2014-12-21 22:34:41 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,57 @@
-## Put comments here that give an overall description of what your
-## functions do
+#
+#
+# It's a function that returns a list of functions
+# It stores a Non Invertible Matrix and cached value of its inverse
+# This function contains the following functions
+#  *	setMatrix 
+#  *  getMatrix 
+#  *  cacheInverse
+#  *  getInverse
+makeCacheMatrix <- function(inputmatrix = numeric()) {
+  	cache <- NULL
 
-## Write a short comment describing this function
+	# Stores a Matrix
+  	setMatrix <- function(input){
+	  inputmatrix <<- input
+  	  cache <<- NULL
+	}
 
-makeCacheMatrix <- function(x = matrix()) {
+	# Returns the stored Matrix/ Getter method
+	getMatrix <- function() inputmatrix
 
+	# Caches the inverse Matrix
+	cacheInverse <- function(solveinverse){
+		cache <<- solveinverse
+	}
+
+	# Gets the cached Inverse Matrix
+	getInverse <- function() cache
+	
+	# Returns list of defined functions
+	list(setMatrix = setMatrix, getMatrix = getMatrix,
+   		cacheInverse = cacheInverse, getInverse = getInverse)
 }
 
+#	This function solves the inverse of matrix created by makeCacheMatrix 
+cacheSolve <- function(y, ...) {
 
-## Write a short comment describing this function
+	# Retrieves the cached inverse
+    	inverse <- y$getInverse()
+    	if(!is.null(inverse)){
+	      message("getting cached data")
+      	return(inverse)
+    	}
+	
+	# First checking if the matrix is Invertible
+    	data <- y$getMatrix()
+	determinant <- det(data)
+	if(determinant == 0){
+		message <- message("It is a non Invertible Matrix")
+		return
+	}
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	# Solving inverse of the matrix
+    	inverse <- solve(data, ...)
+    	y$cacheInverse(inverse)
+    	inverse
 }
